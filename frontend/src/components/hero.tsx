832a081e7f600016fad4edf72f12338ea563832f
Angela Petrone <apetrone@hawk.iit.edu>
2022-10-01 21:41:39 -0500

    Hero component text polishing

--- frontend/src/components/hero.tsx
+++ frontend/src/components/hero.tsx
@@ -8,19 +8,18 @@ const Hero = () => {
 
         <h1 className="flex items-center justify-between">
           <span>From: Twitter</span>
-          <span>To: You</span>
+          <span>To: <b className="text-yellow-500">You</b></span>
         </h1>
 
         <h2 className="flex items-center justify-between">
-          <span className="text-yellow-500">LIVE</span>
-          <span>stock sentiment report</span>
+          <span> <b className="text-yellow-500">LIVE</b> stock sentiment report</span>
         </h2>
 
         <p>
           <span>We get you </span>
           <span>money advice</span>
           <span> and save you the only non-monetary currency: </span>
-          <span lassName="text-yellow-500"> your time</span>
+          <span className="text-yellow-500"> your time</span>
           <span>.</span>
         </p>
 
