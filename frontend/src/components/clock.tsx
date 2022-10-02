bb32486c86f6c55860ee6cb633e13e1bb3e8cc2c
StepBroBD <Hi@StepBroBD.com>
2022-10-01 20:23:41 -0400

    frontend

---
+++ frontend/src/components/clock.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+
+const Clock = () => {
+  const currentTime = () =>
+    new Date().toLocaleString("en-US", {
+      hour12: false,
+      month: "long",
+      day: "numeric",
+      year: "numeric",
+    });
+  const [time, setTime] = useState(currentTime);
+  useEffect(() => {
+    let timer = setInterval(() => {
+      setTime(currentTime);
+    }, 1000);
+    return () => clearInterval(timer);
+  }, []);
+
+  return (
+    <div className="flex flex-row items-center justify-between">
+      <p className="text-sm" suppressHydrationWarning={true}>
+        {time}
+      </p>
+    </div>
+  );
+};
+
+export default Clock;
