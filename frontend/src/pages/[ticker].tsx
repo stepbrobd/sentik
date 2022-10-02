ff5e681c25fc73d095b1efe9b11889997190277c
Angela Petrone <apetrone@hawk.iit.edu>
2022-10-02 05:53:52 -0500

    Add style to table

--- frontend/src/pages/[ticker].tsx
+++ frontend/src/pages/[ticker].tsx
@@ -38,14 +38,17 @@ const Ticker = () => {
         slug="/"
       />
 
-      <table className="min-w-full max-w-full divide-y divide-gray-300">
-        <thead className="bg-gray-50">
+
+        <h1 className="mt-40 pt-8 text-center text-4xl text-black leading-4">Tweets</h1>
+
+      <table className="min-w-full max-w-full divide-y divide-gray-300 rounded-full pr-10 pl-10">
+        <thead className="bg-yellow-400">
           <tr className="divide-x divide-gray-200">
             <th
               scope="col"
-              className="py-3.5 pl-4 pr-4 text-left text-sm font-semibold text-gray-900 sm:pl-6"
+              className="py-3.5 pl-4 pr-4 text-left text-sm font-semibold text-black sm:pl-6"
             >
-              Ticker
+              Stock
             </th>
             <th
               scope="col"
@@ -75,10 +78,10 @@ const Ticker = () => {
                   <td className="whitespace-nowrap py-4 pl-4 pr-4 text-sm font-medium text-gray-900 sm:pl-6">
                     {ticker}
                   </td>
-                  <td className="whitespace-nowrap p-4 text-sm text-gray-500">
-                    {item.Content}
+                  <td className="whitespace-wrap p-4 text-sm text-gray-500"> <span> " </span>
+                    {item.Content} <span> " </span>
                   </td>
-                  <td className="whitespace-nowrap p-4 text-sm text-gray-500">
+                  <td className="whitespace-wrap p-4 text-sm text-gray-500">
                     {item.Date}
                   </td>
                   <td className="whitespace-nowrap py-4 pl-4 pr-4 text-sm text-gray-500 sm:pr-6">
