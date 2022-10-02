c387c59439cab6a8d6ead2f6352fa289f7c2d532
Cybergenik <cybergenik@gmail.com>
2022-10-01 23:43:13 -0400

    vaping

--- backend/main/main.go
+++ backend/main/main.go
@@ -2,39 +2,59 @@ package main
 
 import (
 	"fmt"
-	"hackmit/twitter"
 	"log"
 	"strings"
 	"net/http"
 	"time"
 
+	"hackmit/twitter"
+	"hackmit/openai"
 	"github.com/gin-gonic/gin"
 )
 
 type Tickers map[string][]Tweet
 
 type Tweet struct {
-	Id     string `json:id`
-	Ticker string `json:ticker`
-	//Sent		string `json:sentiment`
+	Id      string `json:id`
+	Ticker  string `json:ticker`
+	Sent    string `json:sentiment`
 	Content string `json:content`
 	Date    string `json:date` //UTC Format
 }
 
-func fetchTweet(ticker string) []Tweet {
-	tw := twitter.MakeClient()
-	tweets, err := tw.GetTrendingByTicker(ticker)
+type API struct {
+	twitter		*twitter.TwitterClient
+	openai		*openai.OpenAIClient
+}
+
+func (api *API) fetchTweet(ticker string) []Tweet {
+	tweets, err := api.twitter.SearchRecent(ticker, 50)
 	if err != nil {
 		log.Printf("Error fetching tweets for ticker: %v", ticker)
 		return nil
 	}
-	var res = []Tweet{}
+
+	var texts = []string{}
 	for _, t := range tweets {
+		if t != nil {
+			text := fmt.Sprint(t.(map[string]interface{})["text"])
+			//log.Printf("tweet: %v", text)
+			texts = append(texts, strings.ReplaceAll(text, "\n", " "))
+		}
+	}
+	sentiments := api.openai.SentimentAnalysis(texts)
+	if sentiments == nil{
+		return nil
+	}
+
+	var res = []Tweet{}
+	for i, t := range tweets {
 		if t != nil {
 			res = append(res, Tweet{
 				Id:      fmt.Sprint(t.(map[string]interface{})["id"]),
 				Ticker:  ticker,
-				Content: fmt.Sprint(t.(map[string]interface{})["text"]),
+				Content: texts[i],
+				Sent:	 sentiments[i],
 				Date:    fmt.Sprint(time.Now().UTC()),
 			})
 		}
@@ -42,19 +62,23 @@ func fetchTweet(ticker string) []Tweet {
 	return res
 }
 
-func getTickers(c *gin.Context) {
+func (api *API) getTickers(c *gin.Context) {
 	tickers := strings.Split(c.Param("tickers"), ",")
 	res := make(Tickers)
 	for _, ticker := range tickers {
 		log.Printf("Fetching data for %v...", ticker)
-		res[ticker] = fetchTweet(ticker)
+		res[ticker] = api.fetchTweet(ticker)
 	}
 	c.IndentedJSON(http.StatusOK, res)
 }
 
 func main() {
 	router := gin.Default()
-	router.GET("/tickers/:tickers", getTickers)
+	api := API{
+		twitter: twitter.MakeClient(),
+		openai: openai.MakeClient(),
+	}
+	router.GET("/tickers/:tickers", api.getTickers)
 
 	router.Run("localhost:8080")
 }
