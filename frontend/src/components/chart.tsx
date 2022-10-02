1f31b3d3ba9aeb44549fd2e3870c224032f40877
Cybergenik <cybergenik@gmail.com>
2022-10-02 10:17:17 -0400

    finalizing chart and tweet table integration, restructuring backend

--- frontend/src/components/chart.tsx
+++ frontend/src/components/chart.tsx
@@ -1,101 +1,70 @@
-// Adding in Content for components here
-import { useState, useEffect } from "react";
-// import {View, Text} from "react-nativ"
 import Plot from "react-plotly.js";
-import ChartInfo from "./chartInfo";
 
-import { StarIcon } from"@heroicons/react/24/solid";
-
-type Props = {
-    stockName: string;
-    stockRating: number;
-    numTweets: string;
-  };
-
-
-import { getAllTweets } from "./hooks/getAllTweetsQuery";
-import { useQuery } from "@tanstack/react-query";
-
-type Props = {
-    stockName: string;
+type Data = {
+  Id: string;
+  Ticker: string;
+  Sent: string;
+  Content: string;
+  Date: string;
 };
-  
 
-const ChartComponent = (props:Props) => {
+type Props = {
+  data: Data[];
+};
 
-    //TODO: handle multiple tickers passed
-    const {data, isLoading} = useQuery(['allTweets'], getAllTweets(props.stockName));
+const ChartComponent = (props: Props) => {
+  var pVotes: number = 0;
+  var nVotes: number = 0;
+  var positiveVotesArr: number[] = [];
+  var negativeVotesArr: number[] = [];
+  var totalVotes: number;
 
-    var postiveVotes = [1, 2, 4, 10]
-    var negativeVotes = [1, 5, 6, 9]
-    //Add in reset time here
-        // Fresh Update
+  var rating: number = 5;
 
-    //
-
-    var stockName = "Apple"
-    var stockRating = 5
-    var numTweets = "35"
-    var chartData : any = []
-//   var chartData: any = [
-//     {
-//       x: [1, 2, 3, 4],
-//       y: postiveVotes,
-//       fill: "tozeroy",
-//       type: "scatter",
-//       name: "+ Votes",
-//     },
-//     {
-//       x: [1, 2, 3, 4],
-//       y: negativeVotes,
-//       fill: "tonexty",
-//       type: "scatter",
-//       name: "- Votes",
-//     },
-//   ];
-
-  var list = [];
-  for (var i = 0; i < stockRating; i++) {
-    list.push(
-      <li className="h-[24px] w-[24px]">
-        <StarIcon />
-      </li>
-    );
+  var elapsedTimes: number[] = [];
+  for (i = 0; i < props.data.length; i++) {
+    elapsedTimes.push(i);
   }
 
-  return (
-    <>
-    <div>
-    <Plot className="leading-4 place-content-center"
-      data={data}
-      layout={{ width: 320, height: 240, title: "Stock Name Here" }}
-    />
+  //gain positive & negative votes
+  for (var i = 0; i < props.data.length; i++) {
+    console.log(props.data[i]);
 
-      <p className="text-center leading-4">{props.stockName}</p>
-      {/* The amount of stars through map */}
-
-      <p className=" leading-4 text-center">Rating {stockRating}
-      <ul className=" leading-4 list-none flex h-[24px] w-[24px] flex-row">{list}</ul> </p>
-
-      <p className="leading-4 text-center text-gray-400">({props.numTweets} tweets in the last 5 minutes)</p>
-      </div>
-    </>
-
-    {isLoading ? (
-        <p> ...Loading </p>
-      ) : data ? (
-          data.data.map((tweet) => {
-              //Generate graph data from Tweets
-          })
-          <Plot
-              data={chartData}
-              layout={{ width: 320, height: 240, title: "Stock Name Here" }}
-          />
-      )
+    if (props.data[i].Sent === "positive") {
+      pVotes++;
+    } else if (props.data[i].Sent === "negative") {
+      nVotes++;
     }
+    positiveVotesArr.push(pVotes);
+    negativeVotesArr.push(nVotes);
+  }
+  var chartData: any = [
+    {
+      x: elapsedTimes,
+      y: negativeVotesArr,
+      type: "scatter",
+      name: "-",
+    },
+    {
+      x: elapsedTimes,
+      y: positiveVotesArr,
+      type: "scatter",
+      name: "+",
+    },
+  ];
 
-    <ChartInfo stockName={stockName} stockRating={stockRating} numTweets = {numTweets}/>
-       </>
+  return (
+    <div className="flex flex-col items-center justify-center space-y-2 pt-6 pb-8">
+      <Plot
+        className="place-content-center leading-4"
+        data={chartData}
+        layout={{ width: 520, height: 340, title: props.data[0].Ticker }}
+      />
+
+      <p className="text-center leading-4 text-gray-400">
+        ({props.data.length} tweets in the last 5 minutes)
+      </p>
+    </div>
   );
 };
 
