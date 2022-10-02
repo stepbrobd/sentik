c16ccaceae81aa8830426c2803f66c3742d5ebbd
Mike Marambio <mikemarambio@Mikes-MacBook-Pro-2.local>
2022-10-02 01:12:44 -0400

    react-query

--- frontend/src/components/chart.tsx
+++ frontend/src/components/chart.tsx
@@ -1,15 +1,19 @@
 // Adding in Content for components here
 import { useState, useEffect } from "react";
+// import {View, Text} from "react-nativ"
 import Plot from "react-plotly.js";
 import ChartInfo from "./chartInfo";
+import { getAllTweets } from "./hooks/getAllTweetsQuery";
+import { useQuery } from "@tanstack/react-query";
 
 type Props = {
     stockName: string;
-    stockRating: number;
-    numTweets: string;
-  };
+};
   
 const ChartComponent = (props:Props) => {
+
+    //TODO: handle multiple tickers passed
+    const {data, isLoading} = useQuery(['allTweets'], getAllTweets(props.stockName));
 
     var postiveVotes = [1, 2, 4, 10]
     var negativeVotes = [1, 5, 6, 9]
@@ -21,30 +25,38 @@ const ChartComponent = (props:Props) => {
     var stockName = " Apple"
     var stockRating = 5
     var numTweets = "35"
-
-  var data: any = [
-    {
-      x: [1, 2, 3, 4],
-      y: postiveVotes,
-      fill: "tozeroy",
-      type: "scatter",
-      name: "+ Votes",
-    },
-    {
-      x: [1, 2, 3, 4],
-      y: negativeVotes,
-      fill: "tonexty",
-      type: "scatter",
-      name: "- Votes",
-    },
-  ];
+    var chartData : any = []
+//   var chartData: any = [
+//     {
+//       x: [1, 2, 3, 4],
+//       y: postiveVotes,
+//       fill: "tozeroy",
+//       type: "scatter",
+//       name: "+ Votes",
+//     },
+//     {
+//       x: [1, 2, 3, 4],
+//       y: negativeVotes,
+//       fill: "tonexty",
+//       type: "scatter",
+//       name: "- Votes",
+//     },
+//   ];
 
   return (
     <>
-    <Plot
-      data={data}
-      layout={{ width: 320, height: 240, title: "Stock Name Here" }}
-    />
+    {isLoading ? (
+        <p> ...Loading </p>
+    ) : data ? (
+        data.data.map((tweet) => {
+            //Generate graph data from Tweets
+        })
+        <Plot
+            data={chartData}
+            layout={{ width: 320, height: 240, title: "Stock Name Here" }}
+        />
+    )
+
     <ChartInfo stockName={stockName} stockRating={stockRating} numTweets = {numTweets}/>
        </>
   );
