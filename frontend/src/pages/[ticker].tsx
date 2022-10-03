b5ddd8fc46ca18a066b5f1ba1a523fbbc6398606
StepBroBD <Hi@StepBroBD.com>
2022-10-03 13:40:29 -0400

    name changes

--- frontend/src/pages/[ticker].tsx
+++ frontend/src/pages/[ticker].tsx
@@ -36,10 +36,10 @@ const Ticker = () => {
   return (
     <>
       <Meta
-        title="Sentik - HackMIT 2022"
-        description="Sentik - a HackMIT 2022 project"
+        title="Sentik-Stocks - HackMIT 2022"
+        description="Sentik-Stocks - a HackMIT 2022 project"
         image="/404.webp"
-        keywords="HackMIT, HackMIT 2022, Sentik, Twitter, Stocks"
+        keywords="HackMIT, HackMIT 2022, Sentik-Stocks, Twitter, Stocks"
         slug="/"
       />
       {loading ? null : <Chart data={data} />}
